febb4cfdf36f50fc91b699e343f6be59c6dfcb18
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-10-06 09:15:29 -1200

    privateRoutes-create

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Component/Routes/Routes.jsx'
+import AuthProviders from './Component/Providers/AuthProviders'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <div className="max-w-screen-xl mx-auto">
+    <AuthProviders>
+    <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router} />
     </div>
+    </AuthProviders>
+ 
   </React.StrictMode>,
 )
